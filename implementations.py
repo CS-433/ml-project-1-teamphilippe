71c3333781a589b5556a219bd349666560c53cb3
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-10-11 11:15:33 +0200

    First part of the implementation (missing comments)

--- implementations.py
+++ implementations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """some helper functions for project 1."""
-import csv
 import numpy as np
+
 
 def standardize(x):
     """Standardize the original data set."""
@@ -11,7 +11,55 @@ def standardize(x):
     x = x / std_x
     return x, mean_x, std_x
 
+"""
+Logistic regression related functions
+"""
 
+def logistic_regression_gradient(y, tx, w, *args):
+    sigmoid = 1 / (1 + np.exp(- (tx @ w)))
+    return tx.T @ (sigmoid - (y + 1)/2)
+
+
+def logistic_regression_loss(y, tx, w, *args):
+    xtw = tx @ w    
+    return np.sum(np.log(1 + np.exp(xtw)) - (1 + y) / 2 * xtw)
+
+# Regularised logistic regression
+def reg_logistic_regression_loss(y, tx, w, lambda_):
+    return logistic_regression_loss(y, tx, w) + 0.5 * lambda_ * (w.T @ w)
+
+
+def reg_logistic_regression_gradient(y, tx, w, lambda_):
+    return logistic_regression_gradient(y, tx, w) + lambda_ * w
+
+"""
+Least squares related functions
+"""
+def compute_loss_least_squares(y, tx, w, *args):
+    """Calculate the loss.
+    """
+    N = tx.shape[0]
+    e = y - tx @ w
+    return 1 / (2 * N) * e.T @ e
+
+def compute_gradient_least_squares(y, tx, w, *args):
+    """Compute the gradient."""
+    e = y - tx @ w
+    return - (tx.T @ e) / y.shape[0]
+
+# Regularised least squares
+def compute_gradient_ridge_regression(y, tx, w, lambda_):
+    """Compute the gradient."""
+    e = y - tx @ w
+    return - (tx.T @ e) / y.shape[0] + 2 * lambda_ * w
+
+def compute_loss_ridge(y, tx, w, lambda_):
+    return np.sqrt(2 * (compute_loss_least_squares(y, tx, w) + lambda_ * w.T @ w))
+
+
+"""
+Function used for Stochastic gradient descent
+"""
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
     Generate a minibatch iterator for a dataset.
@@ -36,66 +84,62 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-            
-def compute_gradient_least_squares(y, tx, w, *args):
-    """Compute the gradient."""
-    e = y - tx @ w
-    return - (tx.T @ e) / y.shape[0]
-
-def compute_gradient_ridge_regression(y, tx, w, lambda_):
-    """Compute the gradient."""
-    e = y - tx @ w
-    return - (tx.T @ e) / y.shape[0]+2*lambda_*w
-
-def compute_gradient_logistic_regression(y,tx,w,lambda_):
-    raise NotImplementedError
-    
-def compute_loss_least_squares(y, tx, w):
-    raise NotImplementedError
-    
-def compute_loss_ridge_regression(y, tx, w):
-    raise NotImplementedError
-    
-def compute_loss_logistic_regression(y, tx, w):
-    raise NotImplementedError
 
 
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, loss_function, compute_gradient, lambda_=0):
+def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, compute_loss, compute_gradient, lambda_=0,
+                                batch_size=1):
     """Stochastic gradient descent algorithm."""
     # Define parameters to store w and loss
     ws = [initial_w]
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        loss = compute_loss(y, tx, w)
+        loss = compute_loss(y, tx, w, lambda_)
+        
         for y_, tx_ in batch_iter(y, tx, batch_size=batch_size):
             sgrad = compute_gradient(y_, tx_, w, lambda_)
-
             w = w - gamma * sgrad
 
             # store w and loss
             ws.append(w)
-            
+
         losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l:.10f}, w0={w0:.4f}, w1={w1:.4f}".format(
-          bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        print("Gradient Descent({bi}/{ti}): loss={l:.10f}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss))
+    return w, losses[-1]
 
-    return losses, ws
-
+"""
+Functions to optimise weights using different methods
+"""
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
+    return stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, compute_loss_least_squares, compute_gradient_least_squares, batch_size=y.shape[0])
+
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
-    
-def least_squares(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
-    
-def ridge_regression(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
-    
+    return stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, compute_loss_ridge, compute_gradient_ridge_regression)
+
+
+def least_squares(y, tx):
+    """
+    Calculate the least squares solution directly.
+    """
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    return w, compute_loss_least_squares(y, tx, w)
+
+
+def ridge_regression(y, tx, lambda_):
+    """
+    Calculate the regularised least square solution directly
+    """
+    w = np.linalg.solve(tx.T @ tx + np.identity(tx.shape[1]) * lambda_ * 2 * tx.shape[0], tx.T @ y)
+    return w, compute_loss_ridge(y, tx, w, lambda_)
+
+
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
-    
-def reg_logistic_regression(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
+    return stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, logistic_regression_loss,
+                                       logistic_regression_gradient)
+
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    return stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, reg_logistic_regression_loss,
+                                       reg_logistic_regression_gradient, lambda_)
